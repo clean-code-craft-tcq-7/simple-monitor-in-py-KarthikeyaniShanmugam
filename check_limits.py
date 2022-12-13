520ab4938c196833993a0815fbfa5259f3d36afc
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2022-12-13 19:20:06 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,30 +1,24 @@
 class BatteryChecker():
     def __init__(self) -> None:
-        self.temperature_state = None
-        self.soc_state = None
-        self.charge_rate_state = None
+        self.temperature_state = True
+        self.soc_state = True
+        self.charge_rate_state = True
 
     def check_battery_temperature_abnormality(self,temperature):
         if (temperature < 0 or temperature > 45):
             self.print_abnormal_state('Temperature')
             self.temperature_state = False
-        else:
-            self.temperature_state = True
         return self
     def check_battery_soc_abnormality(self,soc):
         if soc < 20 or soc > 80:
             self.print_abnormal_state('State of Charge')
             self.soc_state = False
-        else:
-            self.soc_state = True
         return self
 
     def check_charge_rate_abnormality(self,charge_rate):
         if charge_rate > 0.8:
             self.print_abnormal_state('Charge rate')
             self.charge_rate_state = False
-        else:
-            self.charge_rate_state = True
         return self
 
     def print_abnormal_state(self,print_item):
