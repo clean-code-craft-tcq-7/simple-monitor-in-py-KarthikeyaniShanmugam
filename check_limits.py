1741ad6de64492fa510c6c1590323eb3fbd11753
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2022-12-13 18:30:50 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,5 +1,32 @@
+def check_battery_temperature_abnormality(temperature):
+  return (temperature < 0 or temperature > 45)
+
+def check_battery_soc_abnormality(soc):
+  return (soc < 20 or soc > 80)
+
+def check_charge_rate_abnormality(charge_rate):
+  return (charge_rate > 0.8)
+
+def print_abnormal_state(print_item):
+  print(f"{print_item} is out of range!")
+
 
 def battery_is_ok(temperature, soc, charge_rate):
+  if check_battery_temperature_abnormality(temperature):
+     print_abnormal_state("Temperature")
+     return False
+  elif check_battery_soc_abnormality(soc):
+    print_abnormal_state("State of Charge")
+    return False
+  elif check_charge_rate_abnormality(charge_rate):
+    print_abnormal_state("Charge rate")
+    return False
+  return True
+
+
+
+
+def battery_is_ok1(temperature, soc, charge_rate):
   if temperature < 0 or temperature > 45:
     print('Temperature is out of range!')
     return False
@@ -11,6 +38,8 @@ def battery_is_ok(temperature, soc, charge_rate):
     return False
 
   return True
+
+
 
 
 if __name__ == '__main__':
