d33c8442280d6b60492cc61590616841011bc380
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2023-01-03 19:06:22 +0530

    Update check_soc_limits.py

--- check_soc_limits.py
+++ check_soc_limits.py
@@ -8,22 +8,29 @@ class SocChecker():
         self.soc_minimum_threshold = 20
         self.soc_maximum_threshold = 80
 
-        self.minimum_warning = self.soc_minimum_threshold + calculate_warning(self.soc_maximum_threshold)
-        self.maximum_warning = self.soc_maximum_threshold - calculate_warning(self.soc_maximum_threshold)
-    
+        self.minimum_warning_value = self.soc_minimum_threshold + calculate_warning(self.soc_maximum_threshold)
+        self.maximum_warning_value = self.soc_maximum_threshold - calculate_warning(self.soc_maximum_threshold)
+
+        self.minimum_warning = False
+        self.maximum_warning = False
     def check_minimum_warning(self,soc):
-        if soc > self.soc_minimum_threshold and soc <= self.minimum_warning:
-            print_minimum_warning('State of Charge')
+        if soc > self.soc_minimum_threshold and soc <= self.minimum_warning_value:
+            self.minimum_warning = True            
     
     def check_maximum_warning(self,soc):
-        if soc > self.maximum_warning and soc <= self.soc_maximum_threshold:
-            print_maximum_warning('State of Charge')
+        if soc > self.maximum_warning_value and soc <= self.soc_maximum_threshold:
+            self.maximum_warning = True
+            
 
+    def print_warning(self):
+        if soc_warning and self.minimum_warning:
+            print_minimum_warning('State of Charge')
+        if soc_warning and self.maximum_warning:
+            print_maximum_warning('State of Charge')    
     def check_battery_soc_abnormality(self,soc):
-        if soc_warning:
-            self.check_minimum_warning(soc)
-            self.check_maximum_warning(soc)
-
+        self.check_minimum_warning(soc)
+        self.check_maximum_warning(soc)
+        self.print_warning()
         if soc < self.soc_minimum_threshold or soc > self.soc_maximum_threshold:
             print_abnormal_state('State of Charge')
             self.soc_state = False
