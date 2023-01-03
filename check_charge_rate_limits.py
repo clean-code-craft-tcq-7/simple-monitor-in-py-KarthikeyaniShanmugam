9aae6e790dfae0dedd35714c8b00725ae0c18d8d
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2023-01-03 19:19:59 +0530

    Update check_charge_rate_limits.py

--- check_charge_rate_limits.py
+++ check_charge_rate_limits.py
@@ -11,12 +11,13 @@ class ChargeRateChecker():
         if charge_rate > self.maximum_warning_value and charge_rate <= self.charge_rate_maximum_threshold:
             self.maximum_warning = True
 
-    def print_warning(self):
+    def print_maximum_warning(self):
         if charge_rate_warning and self.maximum_warning:
             print_maximum_warning('Charge rate')
         
     def check_charge_rate_abnormality(self,charge_rate):
         self.check_maximum_warning(self,charge_rate)
+        self.print_maximum_warning()
         if charge_rate > self.charge_rate_maximum_threshold:
             print_abnormal_state('Charge rate')
             self.charge_rate_state = False
