24f1b2de18bd148eb97039624878eac1d29b3441
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2023-01-03 18:05:08 +0530

    Update check_soc_limits.py

--- check_soc_limits.py
+++ check_soc_limits.py
@@ -11,12 +11,17 @@ class SocChecker():
         self.minimum_warning = self.soc_minimum_threshold + calculate_warning(self.soc_maximum_threshold)
         self.maximum_warning = self.soc_maximum_threshold - calculate_warning(self.soc_maximum_threshold)
 
-    def check_battery_soc_abnormality(self,soc):
+    def check_minimum_warning(self,soc):
         if soc_warning and soc > self.soc_minimum_threshold and soc <= self.minimum_warning:
             print_minimum_warning('State of Charge')
-
+    
+    def check_maximum_warning(self,soc):
         if soc_warning and soc > self.maximum_warning and soc <= self.soc_maximum_threshold:
             print_maximum_warning('State of Charge')
+
+    def check_battery_soc_abnormality(self,soc):
+        self.check_minimum_warning(soc)
+        self.check_maximum_warning(soc)
 
         if soc < self.soc_minimum_threshold or soc > self.soc_maximum_threshold:
             print_abnormal_state('State of Charge')
