9031f3e904082027e1e19243357c5aa3c2ac73fe
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2023-01-03 19:18:12 +0530

    Update check_temperature_limits.py

--- check_temperature_limits.py
+++ check_temperature_limits.py
@@ -16,16 +16,26 @@ class TemperatureChecker():
     
     def check_minimum_warning(self,temperature):
         if temperature > self.temperature_minimum_threshold and temperature <= self.minimum_warning_value:
-            print_minimum_warning('Temperature')
+            self.minimum_warning = True            
 
     def check_maximum_warning(self,temperature):
         if temperature > self.maximum_warning_value and temperature <= self.temperature_maximum_threshold:
-            print_maximum_warning('Temperature')
+            self.maximum_warning = True            
    
+    def print_minimum_warning(self):
+        if temperature_warning and self.minimum_warning:
+            print_minimum_warning('Temperature')
+
+    def print_maximum_warning(self):            
+        if temperature_warning and self.maximum_warning:
+            print_maximum_warning('Temperature')
+
     def check_battery_temperature_abnormality(self,temperature):
-        if temperature_warning:
-            self.check_minimum_warning(temperature)
-            self.check_maximum_warning(temperature)
+        self.check_minimum_warning(temperature)
+        self.check_maximum_warning(temperature)
+        self.print_minimum_warning()
+        self.print_maximum_warning()
+
         if (temperature < self.temperature_minimum_threshold or temperature > self.temperature_maximum_threshold):
             print_abnormal_state('Temperature')
             self.temperature_state = False
