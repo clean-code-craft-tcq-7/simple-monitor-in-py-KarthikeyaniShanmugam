010b1a473fb79ee6d689e86aa0123de77add4615
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2023-01-03 18:00:50 +0530

    Update check_temperature_limits.py

--- check_temperature_limits.py
+++ check_temperature_limits.py
@@ -8,16 +8,23 @@ class TemperatureChecker():
         self.temperature_minimum_threshold = 0
         self.temperature_maximum_threshold = 45
 
-        self.minimum_warning = self.temperature_minimum_threshold + calculate_warning(self.temperature_maximum_threshold)
-        self.maximum_warning = self.temperature_maximum_threshold - calculate_warning(self.temperature_maximum_threshold)
+        self.minimum_warning_value = self.temperature_minimum_threshold + calculate_warning(self.temperature_maximum_threshold)
+        self.maximum_warning_value = self.temperature_maximum_threshold - calculate_warning(self.temperature_maximum_threshold)
 
-    def check_battery_temperature_abnormality(self,temperature):
-        if temperature_warning and temperature > self.temperature_minimum_threshold and temperature <= self.minimum_warning:
+        self.minimum_warning = False
+        self.maximum_warning = False
+    
+    def check_minimum_warning(self,temperature):
+        if temperature_warning and temperature > self.temperature_minimum_threshold and temperature <= self.minimum_warning_value:
             print_minimum_warning('Temperature')
 
-        if temperature_warning and temperature > self.maximum_warning and temperature <= self.temperature_maximum_threshold:
+    def check_maximum_warning(self,temperature):
+        if temperature_warning and temperature > self.maximum_warning_value and temperature <= self.temperature_maximum_threshold:
             print_maximum_warning('Temperature')
-
+   
+    def check_battery_temperature_abnormality(self,temperature):
+        self.check_minimum_warning(temperature)
+        self.check_maximum_warning(temperature)
         if (temperature < self.temperature_minimum_threshold or temperature > self.temperature_maximum_threshold):
             print_abnormal_state('Temperature')
             self.temperature_state = False
